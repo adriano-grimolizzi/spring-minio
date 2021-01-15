50341174ef65662bcd7cc0291c5319df956ecd90
agrimolizzi_adm <agrimolizzi_adm@20202043-ALTEN.prod.altengroup.dir>
2021-01-15 16:03:33 +0100

    small fix

--- src/main/java/com/example/uploadingfiles/FileSystemStorageService.java
+++ src/main/java/com/example/uploadingfiles/FileSystemStorageService.java
@@ -2,21 +2,16 @@ package com.example.uploadingfiles;
 
 import com.example.uploadingfiles.exceptions.MinioFetchException;
 import com.example.uploadingfiles.exceptions.StorageException;
-import com.example.uploadingfiles.exceptions.StorageFileNotFoundException;
 import com.example.uploadingfiles.properties.StorageProperties;
 import io.minio.*;
 import io.minio.errors.*;
 import io.minio.messages.Item;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,7 +19,6 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @Service
@@ -97,47 +91,4 @@ public class FileSystemStorageService{
 				})
 				.collect(Collectors.toList());
 	}
-
-//	public Stream<Path> loadAll() {
-//		try {
-//			return Files.walk(this.rootLocation, 1)
-//					.filter(path -> !path.equals(this.rootLocation))
-//					.map(path -> this.rootLocation.relativize(path));
-//		} catch (IOException e) {
-//			throw new StorageException("Failed to read stored files", e);
-//		}
-//
-//	}
-//
-//	public Path load(String filename) {
-//		return rootLocation.resolve(filename);
-//	}
-//
-//	public Resource loadAsResource(String filename) {
-//		try {
-//			Path file = load(filename);
-//			Resource resource = new UrlResource(file.toUri());
-//			if(resource.exists() || resource.isReadable()) {
-//				return resource;
-//			}
-//			else {
-//				throw new StorageFileNotFoundException("Could not read file: " + filename);
-//
-//			}
-//		} catch (MalformedURLException e) {
-//			throw new StorageFileNotFoundException("Could not read file: " + filename, e);
-//		}
-//	}
-//
-//	public void deleteAll() {
-//		FileSystemUtils.deleteRecursively(rootLocation.toFile());
-//	}
-//
-//	public void init() {
-//		try {
-//			Files.createDirectory(rootLocation);
-//		} catch (IOException e) {
-//			throw new StorageException("Could not initialize storage", e);
-//		}
-//	}
 }
